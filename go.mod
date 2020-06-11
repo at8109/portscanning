9875f8d2fb9ee1e4821ed3539917718e92f42a62
at8109 <at8109555785@gmail.com>
2020-06-10 20:45:28 -0700

    Port Scanner

---
+++ go.mod
@@ -0,0 +1,3 @@
+module githum.com/at8109/portscanning
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/at8109/portscanning/port"
+	
+)
+
+func main(){
+	fmt.Println("Port Scanner in Go")
+	open := port.ScanPort("tcp" , "localhost" , 4000)
+	fmt.Printf("Port Open: %t\n" , open)
+}
